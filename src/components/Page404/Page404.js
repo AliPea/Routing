a2c2396540874c8f01abf6e8743728378b33244f
AliPea <web.alipea@gmail.com>
2019-02-12 12:55:50 -0800

    routing practice

---
+++ src/components/Page404/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const noMatch = () => (
+  <div>
+    <img src="https://gfnc1kn6pi-flywheel.netdna-ssl.com/wp-content/uploads/2018/07/white-pomeranian-long-1024x555.jpg" style={{width: "50%"}} alt=""/>
+    <h1 style={{ textAlign: 'center'}}>Page Not Found</h1>
+  </div>
+
+);
+
+export default noMatch;
